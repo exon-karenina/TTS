ff3ecfdd91783c44f18a1b05dab806c17dd8371c
niunannan <niunannan888@126.com>
2020-02-27 16:44:17 +0800

    同步到服务器

--- TTW/TTW/urls.py
+++ TTW/TTW/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'',include('active.urls')),
+    url(r'',include('advertise.urls')),
+    url(r'',include('information.urls')),
+    url(r'',include('news.urls')),
+    url(r'',include('question.urls')),
+    url(r'',include('talk.urls')),
+    url(r'',include('user.urls')),
+    url(r'',include('utils.urls')),
 ]
